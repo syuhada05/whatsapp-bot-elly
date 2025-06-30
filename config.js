181ba00bd5002df36012e7903c32c0f6291f93a5
syuhada05 <addastorefollower@gmail.com>
2025-06-30 08:30:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
+module.exports = {
+    owner: ['601139137441'],
+    author: 'ᴇʟʟʏ 🫦',
+    botname: 'ᴇʟʟʏʙᴏᴛ 🫦',
 
+    listprefix: ['+', '!', '.', '~', '✓', '/'],
+    pairing_code: true,
+
+    imageUrl: 'https://files.catbox.moe/n5ze5x.jpg',
+
+    mess: {
+        owner: 'Fungsi ini hanya untuk Owner!',
+        admin: 'Fungsi ini hanya untuk Admin Group!',
+        botAdmin: 'Bot perlu dijadikan admin dahulu!',
+        group: 'Fungsi ini hanya boleh digunakan dalam group!',
+        private: 'Fungsi ini hanya boleh digunakan dalam chat peribadi!',
+        prem: 'Fungsi ini hanya untuk pengguna Premium!',
+        wait: 'Sila tunggu sebentar...',
+        error: 'Maaf, ada ralat berlaku!',
+        done: 'Siap ✅'
+    }
+};
